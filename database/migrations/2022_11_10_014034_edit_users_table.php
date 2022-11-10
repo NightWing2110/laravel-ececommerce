1b7641e105733ee9c9c70f2664137d39017a0f9a
Lưu Duy <luuquanghoangdy@gmail.com>
2022-11-10 10:20:34 +0700

    Image Profile

--- database/migrations/2022_11_10_014034_edit_users_table.php
+++ database/migrations/2022_11_10_014034_edit_users_table.php
@@ -14,7 +14,7 @@ class EditUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('image');
+            $table->string('image')->nullable();
         });
     }
 
