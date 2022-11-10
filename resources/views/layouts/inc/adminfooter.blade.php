7c792842e6386223b2db3625116dcb65afd08a7f
Lưu Duy <luuquanghoangdy@gmail.com>
2022-11-10 15:10:21 +0700

    edit blank

--- resources/views/layouts/inc/adminfooter.blade.php
+++ resources/views/layouts/inc/adminfooter.blade.php
@@ -3,30 +3,11 @@
     <nav class="float-left">
       <ul>
         <li>
-          <a href="https://www.creative-tim.com">
+          <h6>
             Creative By NightWing
-          </a>
-        </li>
-        <li>
-          <a href="https://creative-tim.com/presentation">
-            About Us
-          </a>
-        </li>
-        <li>
-          <a href="http://blog.creative-tim.com">
-            Blog
-          </a>
-        </li>
-        <li>
-          <a href="https://www.creative-tim.com/license">
-            Licenses
-          </a>
+          </h6>
         </li>
       </ul>
     </nav>
-    <div class="copyright float-right" id="date">
-      , made with <i class="material-icons">favorite</i> by
-      <a href="https://www.creative-tim.com" target="_blank">Creative By NightWing</a> for a better web.
-    </div>
   </div>
 </footer>
